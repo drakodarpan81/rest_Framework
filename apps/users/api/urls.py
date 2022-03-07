14c38a227f2cb6f5bd9a2966d150587b87727da5
drakodarpan81 <drakodarpan@gmail.com>
2022-03-07 12:35:54 -0700

    Cambio a funciones

---
+++ apps/users/api/urls.py
@@ -0,0 +1,8 @@
+import imp
+from importlib.resources import path
+from django.urls import path
+from apps.users.api.api import user_api_view
+
+urlpatterns = [
+    path('usuario/', user_api_view, name='usuario_api'),
+]
