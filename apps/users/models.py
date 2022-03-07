d0089f681dcd385df586b4e68b362527a0447021
drakodarpan81 <drakodarpan@gmail.com>
2022-03-07 11:31:57 -0700

    first commit

---
+++ apps/users/models.py
@@ -0,0 +1,55 @@
+from email.mime import image
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from simple_history.models import HistoricalRecords
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, username, email, name, last_name, password, is_staff, is_superuser, **extra_fields):
+        user = self.model(
+            username=username,
+            email=email,
+            name=name,
+            last_name=last_name,
+            is_staff=is_staff,
+            is_superuser=is_superuser,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_user(self, username, email, name, last_name, password=None, **extra_fields):
+        return self._create_user(username, email, name, last_name, password, False, False, **extra_fields)
+
+    def create_superuser(self, username, email, name, last_name, password=None, **extra_fields):
+        return self._create_user(username, email, name, last_name, password, True, True, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField("Correo Electrónico",
+                              max_length=255, unique=True)
+    name = models.CharField("Nombres", max_length=255, blank=True, null=True)
+    last_name = models.CharField(
+        "Apellidos", max_length=255, blank=True, null=True)
+    image = models.ImageField("Imagen de perfil", upload_to='perfil/',
+                              height_field=True, width_field=True, max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    historial = HistoricalRecords()
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'name', 'last_name']
+
+    def natural_key(self):
+        return (self.username)
+
+    def __str__(self):
+        return "Usuario {0}, con el Nombre Completo: {1} {2}".format(self.username, self.last_name, self.name)
