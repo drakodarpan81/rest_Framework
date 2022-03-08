883921721640abc987be2d2852a9d9891c508524
drakodarpan81 <drakodarpan@gmail.com>
2022-03-08 09:46:58 -0700

    Add PUT y DELETE'

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,8 +1,10 @@
 import imp
 from importlib.resources import path
 from django.urls import path
-from apps.users.api.api import user_api_view
+from apps.users.api.api import user_api_view, user_detail_api_view
 
 urlpatterns = [
     path('usuario/', user_api_view, name='usuario_api'),
+    path('usuario/<int:pk>/', user_detail_api_view,
+         name='usuario_detail_api_view')
 ]
